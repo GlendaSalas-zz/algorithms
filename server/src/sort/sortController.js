3bab066624a2c4e7b262e74a067c8e438e533683
GlendaSalas <glenda.salas.a@gmail.com>
2019-08-25 21:15:54 -0500

    Implementation of merge sort algorithm with js

--- server/src/sort/sortController.js
+++ server/src/sort/sortController.js
@@ -18,3 +18,52 @@ exports.insertionSort = async (req, res) => {
 		return res.status(500).send(errorGenerate(500, e.message))
 	}
 }
+const merge = async (Arr, start, mid, end)=>{
+	let temp = []
+	let i = start
+	let j = mid + 1
+	let k = 0
+	while(i<= mid && j<= end){
+		if(Arr[i] <= Arr[j]){
+			temp[k] = Arr[i]
+			k += 1
+			i += 1
+		} else {
+			temp[k] = Arr[j]
+			k += 1
+			j += 1
+		}
+	}
+	while(i <= mid){
+		temp[k] = Arr[i]
+		k += 1
+		i += 1
+	}
+	while (j<= end){
+		temp[k] = Arr[j]
+		k += 1
+		j += 1
+	}
+	for (i= start; i<= end; i += 1){
+		Arr[i] = temp [i-start]
+	}
+	return temp
+}
+const mergeSorting = async(list, start, end)=> {
+	if(start < end ){
+		let mid = parseInt((start + end) / 2)
+		mergeSorting(list, start, mid)
+		mergeSorting(list, mid+1, end)
+		return merge(list, start, mid, end)
+	} 
+}
+exports.mergeSort = async (req, res) => {
+	try {
+		const { list } = req.body
+		if (!list) return res.status(406).send(errorGenerate(406, 'The list is required.'))
+		const result = await mergeSorting(list, 0, list.length - 1)
+		res.status(200).send(errorGenerate(200, `${JSON.stringify(result)} was sort.`))	
+	} catch (e) {
+		return res.status(500).send(errorGenerate(500, e.message))
+	}
+}
--- server/src/sort/sortRouter.js
+++ server/src/sort/sortRouter.js
@@ -4,5 +4,6 @@ import sortController from './sortController'
 const router = express.Router()
 
 router.post('/insertion-sort', sortController.insertionSort)
+router.post('/merge-sort', sortController.mergeSort)
 
 module.exports = router
